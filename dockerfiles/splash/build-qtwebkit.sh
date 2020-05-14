0dd2486986bd6c1ea78be83d17023e4440d93888
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2020-05-14 12:16:45 +0000

    no need for && we in bash not in a docker

--- dockerfiles/splash/build-qtwebkit.sh
+++ dockerfiles/splash/build-qtwebkit.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
-mkdir -p /tmp/builds/qtwebkit && \
-cd /tmp/builds && \
-tar xvfJ "$1" --keep-newer-files --strip-components 1 && \
-mkdir -p build && \
-cd build && \
-cmake -G Ninja -DPORT=Qt -DCMAKE_BUILD_TYPE=Release .. && \
-ninja && \
+mkdir -p /tmp/builds/qtwebkit
+cd /tmp/builds
+tar xvfJ "$1" --keep-newer-files --strip-components 1
+mkdir -p build
+cd build
+cmake -G Ninja -DPORT=Qt -DCMAKE_BUILD_TYPE=Release ..
+ninja 
 ninja install
