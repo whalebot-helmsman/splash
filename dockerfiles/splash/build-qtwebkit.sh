dcd400fe29ce7a7025d23250473629f4f1979862
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2020-05-14 12:26:17 +0000

    we didn`t need this directory

--- dockerfiles/splash/build-qtwebkit.sh
+++ dockerfiles/splash/build-qtwebkit.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir -p /tmp/builds/qtwebkit
+mkdir -p /tmp/builds
 cd /tmp/builds
 tar xvfJ "$1" --keep-newer-files --strip-components 1
 mkdir -p build
 cd build
 cmake -G Ninja -DPORT=Qt -DCMAKE_BUILD_TYPE=Release ..
-ninja 
+ninja
 ninja install
